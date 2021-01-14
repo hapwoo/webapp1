0be273ffa8cd11c74b0370f062bfd9aa9fa2b75a
hapwoo <hapwoo@naver.com>
2021-01-14 17:54:31 +0900

    01/14 마무리

--- src/main/java/com/mycompany/webapp/controller/Ch05Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch05Controller.java
@@ -1,10 +1,13 @@
 package com.mycompany.webapp.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,5 +57,49 @@ public class Ch05Controller {
 		}
 		return "ch05/content";
 	}
+	
+	@GetMapping("/method3")
+	public String method3(HttpServletRequest request, HttpServletResponse response) {
+		
+		//쿠키 생성
+		Cookie cookie1 = new Cookie("memberId","white");
+		Cookie cookie2 = new Cookie("loginStatus", "ok");
+		
+		//쿠키를 응답 헤더에 추가해서 클라이언트에게 보내기
+		response.addCookie(cookie1);
+		response.addCookie(cookie2);
+		//응답 본문을 생성하는 뷰 리턴
+		return "ch05/content";
+	}
+	
+	@GetMapping("/method4")
+	public String method4(HttpServletRequest request, HttpServletResponse response) {
+		
+		//브라우저가 요청 헤더에 보낸 쿠키값을 읽기
+		Cookie[] cookieArr = request.getCookies();
+		for(Cookie cookie : cookieArr) {
+			String name = cookie.getName();
+			String value = cookie.getValue();
+			logger.info(name + ": " + value);
+		}
+		//응답 본문을 생성하는 뷰 리턴
+		return "ch05/content";
+		
+	}
+	
+	@GetMapping("/method5")
+	public String method5(
+			@CookieValue("memberId") String memberId, //쿠키이름과 동일한 경우 어노테이션 뒤 괄호 생략가능
+			@CookieValue("loginStatus") String loginStatus) {
+		
+		//브라우저가 요청 헤더에 보낸 쿠키값을 읽기
+			logger.info("memberId"+ ": " + memberId);
+			logger.info("loginStatus"+ ": " + loginStatus);
+		
+		//응답 본문을 생성하는 뷰 리턴
+		return "ch05/content";
+		
+	}
+	
 	
 }
